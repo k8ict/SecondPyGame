468285824854ecbdaac8240cdc7649f0e546091e
Kamil Wardęga <px.pixel.px@gmail.com>
2022-06-09 04:45:34 +0200

    Added assets

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
 import pygame
+from sys import exit
 
 pygame.init()
+clock = pygame.time.Clock()
+screen = pygame.display.set_mode((800,400))
+pygame.display.set_caption('Runner')
+
+while True:
+
+    #handling input
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+
+    #draw and update
+    pygame.display.update()
+    clock.tick(60)
