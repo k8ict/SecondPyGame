8f721b4ead8fea40bbfc8a2eef98e604dfe494a0
Kamil Wardęga <px.pixel.px@gmail.com>
2022-06-10 06:41:13 +0200

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,10 @@ ground_surface = pygame.image.load('graphics/ground.png').convert()
 text_surface = test_font.render('My Game', False, 'Black')
 
 snail_surface = pygame.image.load('graphics/snail/snail1.png').convert_alpha()
-snail_x_pos = 600
+snail_rect = snail_surface.get_rect(bottomleft = (600,300))
+
+player_surface = pygame.image.load('graphics/player/player_walk_1.png').convert_alpha()
+player_rect = player_surface.get_rect(midbottom = (80,300))
 
 while True:
 
@@ -26,11 +29,16 @@ while True:
     screen.blit(sky_surface,(0,0))
     screen.blit(ground_surface,(0,300))
     screen.blit(text_surface,(50,50))
-    snail_x_pos -= 4
-    if snail_x_pos <= -100:
-        snail_x_pos = 800
-    screen.blit(snail_surface,(snail_x_pos,270))
 
+    snail_rect.x -= 4
+    if snail_rect.right <= 0: snail_rect.left = 800
+    screen.blit(snail_surface,snail_rect)
+    screen.blit(player_surface,player_rect)
+
+    if player_rect.colliderect(snail_rect):
+        print('collision')
+
+    
     #update
     pygame.display.update()
     clock.tick(60)
